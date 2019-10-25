c875e2221463b7bd181ec21dc1d0a1e0d05e7b3b
kdegnan1 <katherine.degnan@gmail.com>
2019-10-25 18:03:02 -0400

    take out debuggers

--- app/controllers/api/event_registrations_controller.rb
+++ app/controllers/api/event_registrations_controller.rb
@@ -4,22 +4,22 @@ class Api::EventRegistrationsController < ApplicationController
 
     def create
         @event = Event.find(params[:event_id]) 
-        # debugger
+   
         if !@event.user_ids.include?(current_user.id)
             @event_registration = EventRegistration.create(user_id: current_user.id, event_id: @event.id) 
                 @event = Event.find(params[:event_id]) 
                 render 'api/events/show'
-            # end
+
         else 
             render 'api/events/show'
         end
     end
 
     def destroy 
-        # debugger
+
         @event_registration = EventRegistration.find_by(event_id: params[:event_id], user_id: current_user.id)
         @event = Event.find(params[:event_id]) 
-        # @event = event_registration.event 
+
         @event_registration.destroy 
         render 'api/events/show'
     end 
