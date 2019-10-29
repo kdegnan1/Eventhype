d8d81bc3bef0121071094baab500701f5996c160
“katherinedegnan” <katherine.degnan@gmail.com>
2019-10-29 13:31:00 -0400

    added reg

--- frontend/components /events/event_reg_item_form.jsx
+++ frontend/components /events/event_reg_item_form.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const RegItem = (props) => { 
     return(
-        <div>
-
+        <div className="reg-item">
+            <div className="reg-item-content"> 
+            <img className="reg-photo" src={props.photoUrl} onClick={props.onEventClick} />
+                <p>{props.title}</p>
+                <p>{props.location}</p>
+                <p>{props.start_date}</p> 
+            </div> 
         </div>
     )
 }
