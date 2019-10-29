0e0d2d10b9d1b16ef4e5bec5a41c82a6d46ad54e
kdegnan1 <katherine.degnan@gmail.com>
2019-10-29 16:37:38 -0400

    registration css

--- frontend/components /events/event_reg_form.jsx
+++ frontend/components /events/event_reg_form.jsx
@@ -24,22 +24,24 @@ class EventReg extends React.Component {
                 key={`event-${idx}`}
                 title = {event.title}
                 photoUrl={event.photoUrl}
-                // location={event.location}
+                location={event.location}
                 start_date={event.start_date} 
                 />
                 ) 
             }
-        })     
+        })       
            debugger 
             
             return ( 
             <div className="reg-window">
                 <div className="backsplash">
                     <div className="user-info">
-                        <img className="prof-pic" src={"https://eventhype-pic.s3.us-east-2.amazonaws.com/Screen+Shot+2019-10-29+at+12.53.22+PM.png"}/>
+                        <img className="prof-pic-show" src={"https://eventhype-pic.s3.us-east-2.amazonaws.com/Screen+Shot+2019-10-29+at+12.53.22+PM.png"}/>
                             <div className="name-title-div">
-                                <h2 className="name-title">{currentUser.fname}</h2>
-                                <h2 className="name-title">{currentUser.lname}</h2>
+                                <p className="name-title">{currentUser.fname}</p>
+                                <p className="name-title">{currentUser.lname}</p>
+                            {/* <p># events</p>
+                            <p># likes</p> */}
                             </div> 
                     </div>
                     <p className="title-event">Events</p>
--- frontend/components /events/event_reg_item_form.jsx
+++ frontend/components /events/event_reg_item_form.jsx
@@ -5,10 +5,12 @@ const RegItem = (props) => {
         <div className="reg-item">
             <img className="reg-photo" src={props.photoUrl} onClick={props.onEventClick} />
             <div className="reg-item-content"> 
-                <p className="reg-title">{props.title}</p>
+                <p className="reg-title">{props.title}</p> 
                 <p className="reg-start">{props.start_date}</p> 
+                <p className="reg-location">{props.location}</p>  
             </div> 
         </div>
     )
 }
+
 export default RegItem
