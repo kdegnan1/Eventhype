210512be6a439f6b48eb35cdad492345edb93c5b
kdegnan1 <katherine.degnan@gmail.com>
2019-10-29 15:26:00 -0400

    registration

--- frontend/components /events/event_reg_item_form.jsx
+++ frontend/components /events/event_reg_item_form.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const RegItem = (props) => { 
     return(
         <div className="reg-item">
-            <div className="reg-item-content"> 
             <img className="reg-photo" src={props.photoUrl} onClick={props.onEventClick} />
-                <p>{props.title}</p>
-                <p>{props.location}</p>
-                <p>{props.start_date}</p> 
+            <div className="reg-item-content"> 
+                <p className="reg-title">{props.title}</p>
+                {/* <p className="reg-location">{props.location}</p> */} 
+                <p className="reg-start">{props.start_date}</p> 
             </div> 
         </div>
     )
