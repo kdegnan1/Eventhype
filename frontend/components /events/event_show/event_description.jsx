ed4c3ed7978e13f47868c92983c5cb13b2ce0953
kdegnan1 <katherine.degnan@gmail.com>
2019-10-30 17:07:58 -0400

    fixed reg

--- frontend/components /events/event_show/event_description.jsx
+++ frontend/components /events/event_show/event_description.jsx
@@ -1,6 +1,15 @@
 import React from 'react' 
 
 const BodyDesc = props => { 
+
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+    let startdate = new Date(props.start_date) 
+    let starttime = new Date(props.start_time) 
+    let enddate = new Date(props.end_date) 
+    let endtime = new Date(props.end_time) 
+
+    debugger 
     return ( 
         <div> 
         <h2 className="header-title">About this Event </h2>
@@ -8,8 +17,8 @@ const BodyDesc = props => {
             <p className="super-desc">{props.description} </p>
             <div className="side-desc-info"> 
                 <h3>Date and Time </h3>
-                    <div> <p>{props.start_date}</p>{props.start_time}- </div>
-                    <div> <p>{props.end_date}</p>{props.end_time} </div>
+                    <div> Start: <p>{props.start_date}</p>{props.start_time}- </div>
+                    <div> End: <p>{props.end_date}</p>{props.end_time} </div>
                 <h3> Location </h3> 
                     <span>{props.location}</span> 
             </div>
