8dca28014a75844fcc474279c52158a069b3e82d
kdegnan1 <katherine.degnan@gmail.com>
2019-10-30 15:05:44 -0400

    added footer

--- frontend/components /App.jsx
+++ frontend/components /App.jsx
@@ -8,6 +8,7 @@ import EventFormContainer from '../components /events/event_form_container'
 import EventIndexContainer from '../components /events/event_index_container'
 import EventShowContainer from '../components /events/event_show/event_show_container'
 import EventRegContainer from '../components /events/event_reg_form_container'
+import Footer from './footer'
 import {Route} from 'react-router-dom' ;
 import { AuthRoute, ProtectedRoute } from '../util /route.util';
 
@@ -15,6 +16,7 @@ export default () => (
     <div>
 
         <Route path='/' component={NavBarContainer}  />
+        <Route path='/' component={Footer} />
         <Route exact path='/' component={Splash}  />
         <Route exact path ='/' component={EventIndexContainer}/>
         {/* <AuthRoute path='/signup' component={SignUpContainer} /> */}
