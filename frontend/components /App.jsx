d1988b6bf95af76331bbaa5484e38cf4f2e94144
kdegnan1 <katherine.degnan@gmail.com>
2019-11-06 16:31:01 -0500

    update

--- frontend/components /App.jsx
+++ frontend/components /App.jsx
@@ -8,6 +8,7 @@ import EventFormContainer from '../components /events/event_form_container'
 import EventIndexContainer from '../components /events/event_index_container'
 import EventShowContainer from '../components /events/event_show/event_show_container'
 import EventRegContainer from '../components /events/event_reg_form_container'
+import EventUpdateFormContainer from '../components /events/event_show/event_update_form_container'
 import Footer from './footer'
 import {Route} from 'react-router-dom' ;
 import { AuthRoute, ProtectedRoute } from '../util /route.util';
@@ -25,6 +26,7 @@ export default () => (
         <AuthRoute path='/signin/signup' component={SignUpContainer} />
         <ProtectedRoute path='/create_event' component={EventFormContainer} /> 
         <ProtectedRoute path='/user_events' component={EventRegContainer} />
+        <ProtectedRoute path='/events/:eventId/update' component={EventUpdateFormContainer}/> 
         <Route path='/' component={Footer} />
     </div>
 )
