c2e74cbaacdf053bf766cb1f99f4b624b5efe6c5
kdegnan1 <katherine.degnan@gmail.com>
2019-11-08 12:46:42 -0500

    categories

--- frontend/components /events/event_index.jsx
+++ frontend/components /events/event_index.jsx
@@ -5,7 +5,7 @@ class EventIndex extends React.Component {
     constructor(props) { 
         super(props); 
             this.state = { 
-
+                category: "All Categories"
             }
             this.handleEventClick = this.handleEventClick.bind(this) 
     }
@@ -22,6 +22,10 @@ class EventIndex extends React.Component {
 
     render() { 
         let rows = this.props.events.map((event, idx) => { 
+            if (
+                this.state.category === "All Categories" ||
+                event.category === this.state.category
+            ) {
             let {location, title, photoUrl, id, start_date} = event; 
             return (
                 <IndexItem
@@ -33,18 +37,29 @@ class EventIndex extends React.Component {
                     onEventClick = {this.handleEventClick(id)} 
                 />
             )
+            }
         });
 
         return ( 
             <div className="index-background"> 
                 <div className="index-container"> 
-                
-                <div className="index-header"> <p className="popular">Popular in</p> <p className="newyork"> New York </p> </div> 
-                    <div className="events-grid"> 
-                        <ul className="rows">
-                            {rows} 
-                        </ul>
-                    </div> 
+                    <div className="index-header"> <p className="popular">Popular in</p> <p className="newyork"> New York </p> </div> 
+                <div className="select-category">
+                    <select className="selector" value={this.state.category} onChange={this.update("category")}>
+                            <option value="All Categories">All Categories</option>
+                            <option key={1}>Concert</option>
+                            <option key={2}>Brunch</option>
+                            <option key={3}>Festival</option>
+                            <option key={4}>Nightlife</option>
+                            <option key={5}>Food</option>
+                            <option key={6}>Sport Event</option>
+                    </select>
+                </div>
+                        <div className="events-grid"> 
+                            <ul className="rows">
+                                {rows} 
+                            </ul>
+                        </div> 
                 </div>
             </div> 
         )
