8825f4e7a063e1f10bdd7002d1d237f85a7d3365
kdegnan1 <katherine.degnan@gmail.com>
2019-11-08 13:10:39 -0500

    removed cols

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_04_190033) do
+ActiveRecord::Schema.define(version: 2019_11_08_180528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,11 +57,9 @@ ActiveRecord::Schema.define(version: 2019_11_04_190033) do
     t.string "description"
     t.string "event_type", null: false
     t.string "price"
-    t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "organizer"
-    t.string "category"
     t.date "start_date"
     t.date "end_date"
     t.time "start_time"
